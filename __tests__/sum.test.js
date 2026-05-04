094ac560103cb9807e3c61acfbf3f014d7f5e713
Bishal khatri <Lahsibkhatri@gmail.com>
2026-05-03 20:53:49 -0700

    test error?

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -2,5 +2,10 @@
 
 import { sum } from '../code-to-unit-test/sum';
 test('adds 1 + 2 to equal 3', () => {
+<<<<<<< HEAD
     expect(1 + 2).toBe(3);
+=======
+    expect(1 + 2).toBe(4);
+    
+>>>>>>> e6a04ee6d6bfbfbed3a74241e854a3d2c785882e
 });
