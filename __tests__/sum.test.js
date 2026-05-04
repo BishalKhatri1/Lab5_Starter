0f6c6fbd0f21e10661f5de6712298b49614e204e
BishalKhatri1 <142729392+BishalKhatri1@users.noreply.github.com>
2026-05-03 21:15:37 -0700

    Fix test expectation for sum function

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -2,10 +2,6 @@
 
 import { sum } from '../code-to-unit-test/sum';
 test('adds 1 + 2 to equal 3', () => {
-<<<<<<< HEAD
     expect(1 + 2).toBe(3);
-=======
-    expect(1 + 2).toBe(4);
-    
->>>>>>> e6a04ee6d6bfbfbed3a74241e854a3d2c785882e
+
 });
